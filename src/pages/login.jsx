c59110bb2171c3d0fe845d8013ee090d977b1301
Hrushi01 <hrushikesh.ambike@gmail.com>
2022-02-25 11:09:38 +0530

    react-router added and pages made

---
+++ src/pages/login.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import "./styles/login.css";
+export default function login() {
+  return <div className="container">login</div>;
+}
---
+++ src/pages/signup.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./styles/login.css";
+
+export default function Signup() {
+  return <div className="container">Signup</div>;
+}
